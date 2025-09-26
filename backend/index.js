96a6dc4c15aad740bbaf7ff6e99ada59c188b96e
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-09-26 14:38:54 -0300

    Fixed backend index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-onst express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
 
 app.listen(port, () => {
-  console.log(`Echelon backend listening on port ${port}`)
-})
+  console.log(`Echelon backend listening on port ${port}`);
+});
